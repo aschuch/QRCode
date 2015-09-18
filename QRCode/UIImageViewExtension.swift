af7b19d0df0561a51f701f98b769bb8b5d326476
Jim Schultz <boxenjim@gmail.com>
2015-09-18 12:02:03 -0600

    swift2 compatible, added guard statements to ciImage, fixed failing tests, updated .travis.yml

--- QRCode/UIImageViewExtension.swift
+++ QRCode/UIImageViewExtension.swift
@@ -12,7 +12,7 @@ public extension UIImageView {
     
     /// Creates a new image view with the given QRCode
     ///
-    /// :param: qrCode      The QRCode to display in the image view
+    /// - parameter qrCode:      The QRCode to display in the image view
     public convenience init(qrCode: QRCode) {
         self.init(image: qrCode.image)
     }
