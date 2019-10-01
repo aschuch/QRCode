162a1666ec6e41efc7529592e96514d6b2bc32cc
Alex Lopez <alex_lopez.es@icloud.com>
2019-10-01 12:29:12 +0200

    Update to Swift 5.0

--- QRCode/UIImageViewExtension.swift
+++ QRCode/UIImageViewExtension.swift
@@ -13,7 +13,7 @@ public extension UIImageView {
     /// Creates a new image view with the given QRCode
     ///
     /// - parameter qrCode:      The QRCode to display in the image view
-    public convenience init(qrCode: QRCode) {
+    convenience init(qrCode: QRCode) {
         self.init(image: qrCode.image)
     }
     
